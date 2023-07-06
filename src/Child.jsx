56e4d92ecf9d40a202045531292518dbe35572c6
vagrant <vagrant@localhost>
2023-07-06 13:41:07 +0900

    router

---
+++ src/Child.jsx
@@ -0,0 +1,8 @@
+function Child() {
+  throw this;
+  return (
+    <div>child page</div>
+  );
+}
+
+export default Child;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Child from "./Child";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>root error!</div>
+  },
+  {
+    path: "/child",
+    element: <Child />
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+/*
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

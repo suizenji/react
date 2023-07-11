4cccc60c02c55ec75acfbc98055060bc5f2f0858
vagrant <vagrant@localhost>
2023-07-11 13:12:59 +0900

    delay

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,5 +1,9 @@
 import {useCount} from "./Contexts/CountContext";
 
+function delay() {
+  return new Promise(resolve => setTimeout(resolve, 1000));
+}
+
 function Counter() {
   const {count, incr} = useCount();
 

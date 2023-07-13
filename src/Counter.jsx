263bfc2e1e1b8881b80a0892b7c9e8d1b041825b
vagrant <vagrant@localhost>
2023-07-13 13:26:11 +0900

    loading

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,4 +1,5 @@
 import {useCount} from "./Contexts/CountContext";
+import {useState} from "react";
 
 function delay() {
   return new Promise(resolve => setTimeout(resolve, 1000));
@@ -6,18 +7,24 @@ function delay() {
 
 function Counter() {
   const {count, incr} = useCount();
+  const [isDone, setDone] = useState(true);
 
   function delayCount() {
-    delay().then(() => incr());
+    setDone(false);
+    delay().then(() => incr()).then(() => setDone(true));
   }
 
-  return (
-    <div>
-      <button onClick={incr}>sync</button>
-      <button onClick={() => delayCount()}>async</button>
-      <span>count is {count}</span>
-    </div>
-  );
+  if (isDone) {
+    return (
+      <div>
+        <button onClick={incr}>sync</button>
+        <button onClick={() => delayCount()}>async</button>
+        <span>count is {count}</span>
+      </div>
+    );
+  } else {
+    return <div>please wait...</div>;
+  }
 }
 
 export default Counter;
